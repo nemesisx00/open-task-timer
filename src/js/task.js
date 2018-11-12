47adbb9b65c2143cfcbe15b420794ca3e8f95bef
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2018-11-12 00:22:11 -0500

    First draft on displaying Time Spans from the selected Task in the View Task window

--- src/js/task.js
+++ src/js/task.js
@@ -1,3 +1,92 @@
 'use strict'
 
 require('../init')
+const {ipcRenderer} = require('electron')
+const moment = require('moment')
+require('moment-duration-format')
+const sprintf = require('sprintf-js').sprintf
+
+const OneTimeEvents = load('event/OneTimeEvents')
+
+global.task = null
+
+const dateFormat = 'Y-MM-DD'
+const timeFormat = 'HH:mm A'
+
+const dayTemplate = `
+			<div class="day shrink">
+				<div class="label shrink">%s</div>
+%s
+			</div>
+`
+
+const timespanTemplate = `
+				<div class="timeSpan shrink%s">
+					<div class="startTime">%s</div>
+					<div class="spacer shrink">-</div>
+					<div class="endTime">%s</div>
+					<div class="duration grow">%s</div>
+				</div>
+`
+
+function parseTimeSpans(task)
+{
+	let parsed = {}
+	if(task.spans)
+	{
+		for(let span of task.spans)
+		{
+			let start = moment(span._start)
+			let date = start.format(dateFormat)
+			let end = null
+			if(span._end)
+				end = moment(span._end)
+			
+			let duration = moment.duration(end.diff(start))
+			
+			if(!parsed[date])
+				parsed[date] = []
+			
+			parsed[date].push({
+				start: start.format(timeFormat),
+				end: end ? end.format(timeFormat) : 'N/A',
+				duration: `${duration.hours()} hours ${duration.minutes()} minutes`
+			})
+		}
+	}
+	
+	return parsed
+}
+
+function buildDayHtml(date, spans)
+{
+	let alt = true
+	let spanHtml = []
+	for(let span of spans)
+	{
+		alt = !alt
+		spanHtml.push(sprintf(timespanTemplate, alt ? ' alt' : '', span.start, span.end, span.duration))
+	}
+	
+	return sprintf(dayTemplate, date, spanHtml.join('\n'))
+}
+
+ipcRenderer.on(OneTimeEvents.viewTask, (event, args) => {
+	if(args && args.task)
+	{
+		let parsed = parseTimeSpans(args.task)
+		
+		let html = ''
+		for(let date in parsed)
+		{
+			if(parsed[date])
+			{
+				if(html)
+					html += '\n'
+				html += buildDayHtml(date, parsed[date])
+			}
+		}
+		
+		document.getElementById('container').innerHTML = html
+	}
+})
---
+++ src/lib/event/OneTimeEvents.js
@@ -0,0 +1,5 @@
+'use strict'
+
+module.exports = Object.freeze({
+	viewTask: 'one-time-view-task'
+})
